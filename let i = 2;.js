89cf12fc6a484273294969417399bfbb596c5211
Viktoria <8ik3oria@gmail.com>
2022-07-20 23:53:23 +0300

    practice task

---
+++ let i = 2;.js
@@ -0,0 +1,8 @@
+let i = 3;
+
+
+while (i <= 16 && i%2 != 0) {   
+    console.log('iii',i);
+    i+=2;
+    console.log('i',i);
+}
---
+++ practice_cycle.js
@@ -0,0 +1,56 @@
+//Задача 1
+
+for (let i = 5; i <= 10; i++) {
+    console.log(i);
+}
+
+//Задача 2
+
+for (let i = 20; i <= 20; i--) {
+    if (i === 13) {
+        break;
+    }
+    console.log(i);
+}
+
+//Задача 3
+
+for (let i = 2; i <= 10; i++) {
+    if (i%2 == 0) {
+        console.log(i);
+    } 
+        continue;
+}
+
+//Задача 4
+
+for (let i = 2; i <= 16; i++) {
+    if (i%2 === 0) {
+        continue;
+    } else {
+        console.log(i);
+    }
+    
+}
+
+//вариант с while
+
+let i = 2;
+
+while (i++ < 16 && i%2 != 0) {   
+    console.log('iii',i);
+    i++;
+    console.log('i',i);
+} 
+
+//задача 5
+
+const arr = [];
+let j = 0;
+
+for (let i = 5; i <= 10; i++) {
+    arr[j] = i;
+    j++;
+}
+console.log(arr);
+
