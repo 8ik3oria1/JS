7a5eaaa83fab215d63d009f6f8a4f2cfab75b92c
Viktoria <8ik3oria@gmail.com>
2022-08-23 21:28:10 +0300

    about return again

--- script_2_26.js
+++ script_2_26.js
@@ -9,3 +9,57 @@ function convert (amount, curr) {
 
 convert(500, usdCurr);
 convert(500, eurCurr);
+
+//про важность return
+
+const usdCurr = 28;
+const discount = 0.9;
+
+function convert (amount, curr) {
+    return curr * amount; //возвращает какой-то результат который можно использовать дальше 
+    //и соответственно здесь функция и завершиться!!!! все что дальше работать не будут
+}
+
+function promotion(result) {
+    console.log(result*discount);
+}
+
+const res = convert(500, usdCurr);
+promotion(res);
+//либо можно так 
+//promotion(convert(500, usdCurr));
+
+//когда функция возвращает другую функцию
+
+const usdCurr = 28;
+const discount = 0.9;
+
+function convert (amount, curr) {
+    return curr * amount; //возвращает какой-то результат который можно использовать дальше 
+    //и соответственно здесь функция и завершиться!!!! все что дальше работать не будут
+}
+
+function promotion(result) {
+    console.log(result*discount);
+    return function(){} //вот здесь функция возвращает функцию
+}
+
+const res = convert(500, usdCurr);
+promotion(res);
+
+//еще один вариант когда return без значения
+
+function test() {
+    for (let i = 0; i<5; i++){
+        console.log(i);
+        if (i === 3) return //фактически это если i = 3 заверши функцию и под капотом вовращается undefined
+    }
+    console.log('done');
+}
+
+test(); //вызов самой функции
+
+//еще одна функция
+
+function doNothing(){};
+console.log(doNothing() === undefined);
