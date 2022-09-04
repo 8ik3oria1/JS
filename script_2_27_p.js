adc9f447544d32e9701417cd171cd0a76d57741d
Viktoria <8ik3oria@gmail.com>
2022-09-04 19:49:10 +0300

    example from teacher

--- script_2_27_p.js
+++ script_2_27_p.js
@@ -42,3 +42,40 @@ function getMathResult(x, y) {
 
 
 console.log(getMathResult(5, 3));
+
+//решение препода
+
+function sayHello(name) {
+    return `Привет, ${name}!`;
+}
+
+sayHello('Alex');
+
+function returnNeighboringNumbers(num) {
+    return [num - 1, num, num + 1];
+}
+
+returnNeighboringNumbers(5);
+
+function getMathResult(num, times) {
+    if (typeof(times) !== 'number' || times <= 0) {
+        return num;
+    }
+
+    let str = '';
+
+    for (let i = 1; i <= times; i++) {
+        if (i === times) {
+            str += `${num * i}`;
+            // Тут без черточек в конце
+        } else {
+            str += `${num * i}---`;
+            // Это тоже самое, что и
+            // str = str + num * i + "---"
+        }
+    }
+
+    return str;
+}
+
+getMathResult(10, 5);
